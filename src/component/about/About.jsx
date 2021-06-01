5ec2a32df4d37f13c66caa8b997bec889f0bee4c
rajeev8620 <kumarrajeev.191@gmail.com>
2021-06-01 22:43:31 +0530

    "resolved conflict"

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -3,12 +3,8 @@ import './about.css';
 export default function About() {
     return (
         <div>
-<<<<<<< HEAD
-        <h3>About component....123</h3> 
-=======
         <h3>About component....</h3> 
-        <p>Checking for conflict</p>
->>>>>>> c178e408dd82084af92ab1ad9e666a5d180b0496
+        <p>Checking for conflict..</p>
         </div>
     )
 }
